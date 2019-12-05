618de84c17ee12b82973b4877ffc8abd4a4b779b
Anthony <8753187+bigfoott@users.noreply.github.com>
2019-12-05 02:41:40 -0500

    updated to post once every hour

--- CronchBot/Program.cs
+++ CronchBot/Program.cs
@@ -33,7 +33,7 @@ namespace CronchBot
             while (true)
             {
                 Thread.Sleep(1000);
-                if (DateTime.Now.Minute % 30 == 0)
+                if (DateTime.Now.Minute == 0)
                 {
                     if (hasPosted) continue;
                     hasPosted = true;
